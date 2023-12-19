face8e13426fa1364d22e1df3614b7127ea63422
Francis Pion <francispion@hotmail.com>
2023-12-19 14:14:04 -0500

    Installed Bootstrap.

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,15 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'About' }">About</RouterLink>
+      </li>
+    </ul>
+  </nav>
   <RouterView />
 </template>
